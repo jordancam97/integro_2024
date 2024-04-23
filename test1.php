6765df2f0d7e43e582ee8b5b2f4bee1c3a76205c
Jordancam97 <jordangw97@gmail.com>
2024-04-22 23:16:39 -0500

    test1 completed

--- test1.php
+++ test1.php
@@ -1,8 +1,10 @@
 <?php
 
 function is_palindrome($input) {
-    //solve this method that says if the sentence is palindrome or not
+    $input = strtolower(preg_replace("/[^a-zA-Z0-9]/", "", $input));
+    $reversed = strrev($input);
+    return $input === $reversed;
 }
 
 $input = "Allí por la tropa portado, traído a ese paraje de maniobras, una tipa como capitán usar boina me dejara, pese a odiar toda tropa por tal ropilla";
-var_dump(is_palindrome($input));
+var_dump(is_palindrome($input)); 
